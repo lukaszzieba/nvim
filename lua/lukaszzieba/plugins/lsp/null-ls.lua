25ccee262776bffa8c8f569d0a46fa939ff860f5
Łukasz Zięba <you@example.com>
2024-02-03 14:56:53 +0100

    Eslnit

--- lua/lukaszzieba/plugins/lsp/null-ls.lua
+++ lua/lukaszzieba/plugins/lsp/null-ls.lua
@@ -20,12 +20,12 @@ null_ls.setup({
 		formatting.prettier, -- js/ts formatter
 		formatting.stylua, -- lua formatter
 		--- eslint_d
-		-- diagnostics.eslint_d.with({ -- js/ts linter
-		-- 	-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-		-- 	condition = function(utils)
-		-- 		return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-		-- 	end,
-		-- }),
+		diagnostics.eslint_d.with({ -- js/ts linter
+			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
+			condition = function(utils)
+				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something elseeslint
+			end,
+		}),
 	},
 	-- configure format on save
 	on_attach = function(current_client, bufnr)
